cd32ca2d6c49ebee6e5c25de59ec5ab24d085b9c
vaisakhn <vaisakhn02@gmail.com>
2024-02-12 21:23:21 +0530

    line swapped

--- src/LayoutRoutes/Routes.js
+++ src/LayoutRoutes/Routes.js
@@ -12,8 +12,9 @@ function RoutesLayout() {
    <Router>
     <Header/>
     <Routes>
+    <Route path='/VK-Store' element={<Home/>}></Route>
         <Route path='/' element={<Home/>}></Route>
-        <Route path='/VK-Store' element={<Home/>}></Route>
+        
         <Route path='/logout' element={<Logout/>}></Route>
         <Route path='/product/:id' element={<ProductDetails/>}></Route>
 
